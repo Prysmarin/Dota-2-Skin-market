19387265490b4b2d9eb394162234ed5decb8f054
Prysm <kantkikik2@gmail.com>
2024-12-13 16:55:36 +0000

    png image

--- main.js
+++ main.js
@@ -2,23 +2,23 @@
 const skins = {
     "claszian-apostasy": {
       name: "Claszian Apostasy",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DjBcDf4FthB8&psig=AOvVaw1GT7NzaQU0lgFg2fiH-Jkj&ust=1734192200828000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOibp8KPpYoDFQAAAAAdAAAAABAI",
+      img: "Void.png",
     },
     "abberant-observer": {
       name: "Abberant Observer",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhawk.live%2Fru%2Fposts%2Fcrownfall-sokrovishnica-pudge-aberrant-observer&psig=AOvVaw2N1iz_0b1YABsXckbidUv9&ust=1734192232518000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiFptGPpYoDFQAAAAAdAAAAABAP",
+      img: "pudge.png",
     },
     "starlorn-adjudicator": {
       name: "Starlorn Adjudicator",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.olx.ua%2Fd%2Fobyavlenie%2Fstarlorn-adjudicator-dota-2-sety-IDVnDme.html&psig=AOvVaw1g9-HpCjjnr5NkUQ8i8dwx&ust=1734192271788000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC93-SPpYoDFQAAAAAdAAAAABAJ",
+      img: "Dawnbraker.png",
     },
     "undying-love": {
       name: "Undying Love",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsteamcommunity.com%2Fsharedfiles%2Ffiledetails%2F%3Fl%3Dukrainian%26id%3D3096946990&psig=AOvVaw0kxC3rI0P2PoS7vtmxtQIY&ust=1734192146425000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOCcvqiPpYoDFQAAAAAdAAAAABAE",
+      img: "Undying.png",
     },
     "mocking-bird": {
       name: "Mocking Bird",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbattlepass.ru%2Fservice%2Fdota_mall&psig=AOvVaw3vXCK0k9frk8qKLZWAjKou&ust=1734192173762000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKj_mLWPpYoDFQAAAAAdAAAAABAHhttps://via.placeholder.com/200?text=Mocking+Bird",
+      img: "mocking-bird.png",
     },
   };
   
